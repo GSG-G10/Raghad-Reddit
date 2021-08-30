707631214118323aee775ebbe477f7e3ccfe9e53
Raghad-Mezied <rmezied3@gmail.com>
2021-08-30 13:01:19 +0300

    Write functions to check cookies and add username for logged in users

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const { handleSignUp, handleSignIn, addToCookies } = require('../controllers');
+const cookieParse = require('cookie-parser');
+const {
+  handleSignUp, handleSignIn, addToCookies, checkUser,
+} = require('../controllers');
 
+router.use(cookieParse());
 router.post('/sign-up', handleSignUp, addToCookies);
 router.post('/sign-in', handleSignIn, addToCookies);
+router.get('/check-user', checkUser);
 
 module.exports = router;
