3b07222c64845768f4a359265fac234db864e854
Raghad-Mezied <rmezied3@gmail.com>
2021-08-30 19:14:14 +0300

    write a query to get all communities

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -2,7 +2,8 @@ const signUpQuery = require('./signUpQuery');
 const signInQuery = require('./signInQuery');
 const getTopPostToday = require('./getTopPostToday');
 const getTopPostNow = require('./getTopNowPosts');
+const getCommunities = require('./getCommunities')
 
 module.exports = {
-  signUpQuery, signInQuery, getTopPostToday, getTopPostNow,
+  signUpQuery, signInQuery, getTopPostToday, getTopPostNow, getCommunities,
 };
