130b1fecd062f0bce6cc0823ae83dc17e167ca49
Raghad-Mezied <rmezied3@gmail.com>
2021-08-30 23:35:31 +0300

    write functions to request communities name and display them in the home page Relates #22

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,8 @@ const handleSignIn = require('./signIn');
 const addToCookies = require('./addToCookies');
 const checkUser = require('./checkUser');
 const handleSignOut = require('./signOut');
+const handleCommunityNames = require('./communityName');
 
 module.exports = {
-  handleSignUp, handleSignIn, addToCookies, checkUser, handleSignOut,
+  handleSignUp, handleSignIn, addToCookies, checkUser, handleSignOut, handleCommunityNames,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const cookieParse = require('cookie-parser');
 const {
-  handleSignUp, handleSignIn, addToCookies, checkUser, handleSignOut,
+  handleSignUp, handleSignIn, addToCookies, checkUser, handleSignOut, handleCommunityNames,
 } = require('../controllers');
 
 router.use(cookieParse());
@@ -9,5 +9,6 @@ router.post('/sign-up', handleSignUp, addToCookies);
 router.post('/sign-in', handleSignIn, addToCookies);
 router.get('/sign-out', handleSignOut);
 router.get('/check-user', checkUser);
+router.get('/community', handleCommunityNames);
 
 module.exports = router;
