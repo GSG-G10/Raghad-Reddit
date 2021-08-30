f3d25f1daa7b8d4d711fade47618646857860cc9
Raghad-Mezied <rmezied3@gmail.com>
2021-08-30 16:22:55 +0300

    write a query to get today's posts ordered by vote Relates 18 Relates #18

---
+++ server/database/queries/getTopPostToday.js
@@ -0,0 +1,3 @@
+const connection = require('../connection');
+
+module.exports = () => connection.query('SELECT * FROM  posts WHERE DATE(post_date) = DATE(NOW()) ORDER BY vote;');
--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,4 +1,6 @@
 const signUpQuery = require('./signUpQuery');
 const signInQuery = require('./signInQuery');
+const getTopPostToday = require('./getTopPostToday');
 
-module.exports = { signUpQuery, signInQuery };
+
+module.exports = { signUpQuery, signInQuery, getTopPostToday };
