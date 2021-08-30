78e3db3beb577ddff04192759621e0271a3e6c34
Raghad-Mezied <rmezied3@gmail.com>
2021-08-30 21:24:22 +0300

    edit dom and fetch.js to diplay username when logged in, write function to fetch user' profile when search by their username, edit sign up handle and handle the sign out to clear cookies Relates #19

--- public/javascript/dom.js
+++ public/javascript/dom.js
@@ -1,3 +1,51 @@
+const searchBtn = document.querySelector('.search-icon');
+const searchInput = document.querySelector('#search-input');
+const navBar = document.querySelector('.home-nav');
+
+// function to create a new tag, give it a class and append it to a parent.
+const createNode = (tag, className, parentNode) => {
+  const tagName = document.createElement(tag);
+  tagName.classList.add(className);
+  parentNode.appendChild(tagName);
+  return tagName;
+};
+
+// function to change sign up and sign in to username and its options
+// eslint-disable-next-line no-unused-vars
+const addUser = (username) => {
+  navBar.textContent = '';
+  const userNameBtn = createNode('button', 'username-btn', navBar);
+  userNameBtn.textContent = username
+  const dropList = createNode('div', 'drop-list', navBar);
+  const profile = createNode('a', 'profile', dropList);
+  profile.textContent = 'Profile';
+  profile.href = `/user/${username}`;
+  const signOut = createNode('a', 'sign-out', dropList);
+  signOut.textContent = 'Sign out';
+  signOut.href = '/sign-out';
+};
+
+// function to get input value and run fetch function
+const getUserProfile = () => {
+  const inputValue = searchInput.value.trim();
+  // eslint-disable-next-line no-undef
+  fetchUserProfile(inputValue);
+};
+
+// function to display some changes in other users profiles
+// eslint-disable-next-line no-unused-vars
+const updateProfilePage = (username, date) => {
+  document.querySelector('.user-name').textContent = username;
+  document.querySelector('.user-date').textContent = date;
+  document.querySelector('.add-post-btn').style.display = 'none';
+  document.querySelector('.saved-btn').style.display = 'none';
+  createNode('button', 'follow-btn', document.querySelector('right-profile-section')).textContent = 'FOLLOW';
+};
+
+searchBtn.addEventListener('click', getUserProfile);
+
+
+
 const signUpBtn = document.querySelector('#sign-up-btn');
 const signInBtn = document.querySelector('#sign-in-btn');
 
@@ -12,21 +60,28 @@ const closeForm = (i) => {
   signPopUp[i].style.display = 'none';
 };
 
-signUpBtn.addEventListener('click', () => { openForm(0); });
-signInBtn.addEventListener('click', () => { openForm(1); });
+signUpBtn.addEventListener('click', () => {
+  openForm(0);
+});
+signInBtn.addEventListener('click', () => {
+  openForm(1);
+});
 closeIcon.forEach((elem, index) => {
-  elem.addEventListener('click', () => { closeForm(index); });
+  elem.addEventListener('click', () => {
+    closeForm(index);
+  });
 });
 
-const createNode = (tag, className, parentNode) => {
-  const tagName = document.createElement(tag);
-  tagName.classList.add(className);
-  parentNode.appendChild(tagName);
-  return tagName;
-};
-
 const postsSection = document.querySelector('.posts-section');
-const createPostContainer = (communityName, userName, userImg, time, title, postString, voteNum) => {
+const createPostContainer = (
+  communityName,
+  userName,
+  userImg,
+  time,
+  title,
+  postString,
+  voteNum,
+) => {
   const postContainer = createNode('section', 'post-container', postsSection);
   const postVote = createNode('section', 'post-vote', postContainer);
   const upVote = createNode('img', 'up-vote', postVote);
@@ -41,7 +96,11 @@ const createPostContainer = (communityName, userName, userImg, time, title, post
   userImage.src = userImg;
   const userInfoText = createNode('p', 'user-info-text', userInfo);
   userInfoText.textContent = `${communityName}`;
-  createNode('span', 'user-info-text', userInfoText).textContent = `Posted by ${userName} ${time}`;
+  createNode(
+    'span',
+    'user-info-text',
+    userInfoText,
+  ).textContent = `Posted by ${userName} ${time}`;
   const postText = createNode('section', 'post-text', post);
   const postTitle = createNode('h3', 'post-title', postText);
   postTitle.textContent = title;
@@ -54,8 +113,16 @@ const createPostContainer = (communityName, userName, userImg, time, title, post
   postSave.textContent = 'Save';
 };
 const CreateCommentContainer = (userName, userImg, text, voteNum) => {
-  const commentsContainer = createNode('section', 'comments-container', postsSection);
-  const commentContainer = createNode('section', 'comment-container', commentsContainer);
+  const commentsContainer = createNode(
+    'section',
+    'comments-container',
+    postsSection,
+  );
+  const commentContainer = createNode(
+    'section',
+    'comment-container',
+    commentsContainer,
+  );
   const userImage = createNode('img', 'user-img', commentContainer);
   userImage.src = userImg;
   const commentUser = createNode('p', 'comment-user', commentContainer);
@@ -74,18 +141,3 @@ const CreateCommentContainer = (userName, userImg, text, voteNum) => {
   const commentSave = createNode('button', 'comment-save', commentOpt);
   commentSave.textContent = 'Save';
 };
-
-const navBar = document.querySelector('.home-nav');
-
-const addUser = (username) => {
-  navBar.textContent = '';
-  createNode('button', 'sign-out-btn', navBar).textContent = 'SIGN OUT';
-  createNode('button', 'username-btn', navBar).textContent = username;
-};
-
-const updateProfilePage = (username, date) => {
-  document.querySelector('.user-name').textContent = username;
-  document.querySelector('.user-date').textContent = date;
-  document.querySelector('.add-post-btn');
-  createNode('button', 'follow-btn', document.querySelector('right-profile-section')).textContent = 'FOLLOW';
-}
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,8 @@ const handleSignUp = require('./signUp');
 const handleSignIn = require('./signIn');
 const addToCookies = require('./addToCookies');
 const checkUser = require('./checkUser');
+const handleSignOut = require('./signOut');
 
 module.exports = {
-  handleSignUp, handleSignIn, addToCookies, checkUser,
+  handleSignUp, handleSignIn, addToCookies, checkUser, handleSignOut,
 };
