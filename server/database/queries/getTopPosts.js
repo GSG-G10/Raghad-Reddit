c12651c5c06d741d70d83cabcab6f804ef8ecdd4
Raghad-Mezied <rmezied3@gmail.com>
2021-08-31 11:06:15 +0300

    edit top posts queries

--- server/database/queries/getTopPosts.js
+++ server/database/queries/getTopPosts.js
@@ -1,3 +1,3 @@
 const connection = require('../connection');
 
-module.exports = () => connection.query('SELECT * FROM posts ORDER BY vote DESC;');
+module.exports = () => connection.query('SELECT posts.id, posts.community_name, posts.post_text, posts.post_date, posts.title, posts.vote, users.username, users.img FROM posts INNER JOIN users ON users.id = posts.user_id ORDER BY vote DESC;');
