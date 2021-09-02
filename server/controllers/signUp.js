fe37ef9825e4f2ccdbc35f80f7769f0ab6e02bfe
Raghad-Mezied <rmezied3@gmail.com>
2021-09-02 14:30:59 +0300

    fix handle sign errors

--- server/controllers/signUp.js
+++ server/controllers/signUp.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
         })
         .catch(() => {
           res.cookie('error', 'The username or the email you entered have been already used');
-          res.redirect('/');
+          res.redirect('/html/signUp.html');
         });
     }
   });
